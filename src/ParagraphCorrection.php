cafe533a00439abb88467cc7c8a0d4fb5ab27843
kouwa stephane <kksasteph888@gmail.com>
2022-12-01 10:15:06 +0100

    Maj

--- src/ParagraphCorrection.php
+++ src/ParagraphCorrection.php
@@ -18,15 +18,29 @@ class ParagraphCorrection {
   static function getEntityUnInstall($paragraph_type_id, $id) {
     $entities = [
       'block_content',
-      'node'
+      'node',
+      'commerce_product',
+      'cv_entity',
+      'model_cv',
+      'site_internet_entity',
+      'site_type_datas'
     ];
-
+    $results = [];
     /**
      * --
      */
-    foreach ($entities as $entity_type_id) {
-      $table = $entity_type_id . '_layout_paragraphs';
-      $results = [];
+    foreach ($entities as $base_table) {
+      $table = $base_table . '__layout_paragraphs';
+      $idColumn = 'id';
+      switch ($base_table) {
+        case 'commerce_product':
+          $idColumn = 'product_id';
+          break;
+
+        case 'node':
+          $idColumn = 'nid';
+          break;
+      }
       /**
        *
        * @var \Drupal\Core\Database\Connection $dateBase
@@ -34,11 +48,20 @@ class ParagraphCorrection {
       $dateBase = \Drupal::database();
       if ($dateBase->schema()->tableExists($table)) {
         $query = $dateBase->select($table, 'n');
+        $query->addJoin('INNER', $base_table . '_field_data', 'bt', 'n.entity_id=bt.' . $idColumn);
+        $query->fields('n', [
+          'bundle',
+          'layout_paragraphs_target_id',
+          'langcode',
+          'entity_id'
+        ]);
+        $query->fields('bt');
+        //
         $query->condition('bundle', $paragraph_type_id);
-        $results[] = $query->execute();
+        $results[] = $query->execute()->fetchAll(\PDO::FETCH_ASSOC);
       }
-      return $results;
     }
+    return $results;
   }
 
 }
