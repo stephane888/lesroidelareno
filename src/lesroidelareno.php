d00a6097f1bf04c270f8b15be2b6ae78bde3c4eb
kouwa stephane <kksasteph888@gmail.com>
2022-12-02 15:52:08 +0100

    Maj

--- src/lesroidelareno.php
+++ src/lesroidelareno.php
@@ -2,10 +2,37 @@
 
 namespace Drupal\lesroidelareno;
 
+use Drupal\commerce_product\Entity\Product;
+use Drupal\Core\Url;
+
 class lesroidelareno {
   
   static public function getCurrentUser() {
     return \Drupal::currentUser()->id();
+  }
+  
+  static public function getRouteVariations(Product $Product) {
+    $link = '';
+    /**
+     *
+     * @var \Drupal\Core\Render\Renderer $renderer
+     */
+    $renderer = \Drupal::service('renderer');
+    if (!empty($Product->id()))
+      $link = [
+        '#type' => 'link',
+        '#title' => t('Editer'),
+        '#url' => Url::fromRoute("entity.commerce_product_variation.collection", [
+          'commerce_product' => $Product->id()
+        ]),
+        '#options' => [
+          'attributes' => [
+            'target' => '_blank',
+            'class' => []
+          ]
+        ]
+      ];
+    return $link;
   }
   
 }
