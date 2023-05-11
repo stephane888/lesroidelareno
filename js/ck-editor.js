8a3a99986487c7207060c5c56ebdc15c662cf77a
kouwa stephane <kksasteph888@gmail.com>
2023-05-11 16:24:13 +0200

    remove orther files of module

--- js/ck-editor.js
+++
@@ -1,7 +0,0 @@
-CKEDITOR.editorConfig = function( config ) {
-	CKEDITOR.config.htmlEncodeOutput = false;
-    CKEDITOR.config.entities = false;
-    CKEDITOR.dtd.$removeEmpty.span = 0;
-    CKEDITOR.dtd.$removeEmpty.i = 0;
-    CKEDITOR.dtd.$removeEmpty.label = 0;
-};
